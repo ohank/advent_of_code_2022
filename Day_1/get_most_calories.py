083f37310f6176973d4190a670260f8a5460004d
Johan Karlsson <karlssonjohan83@gmail.com>
2022-12-04 12:23:58 +0100

    Added calculation for tre units

--- Day_1/get_most_calories.py
+++ Day_1/get_most_calories.py
@@ -1,12 +1,26 @@
 from pathlib import Path
-
-def get_most(data_file: Path)-> int:
+from typing import List
+def convert_data_to_int(str_list: List[List[str]]) -> List[List[int]]:
+        return [[int(y) for y in i] for i in str_list]
+def loade_input_file(data_file) -> List[List[int]]:
     with open(data_file, 'r') as f:
         lines = f.read()
-    elf_sum: dict = {}
-    elf_collection = lines.split('\n\n')
-    elf_collection = [i.split('\n') for i in elf_collection]
-    elf_collection = [[int(y) for y in i] for i in elf_collection]
-    for number, calories in enumerate(elf_collection):
-        elf_sum[number+1] = sum(calories)
-    return max(elf_sum, key=elf_sum.get)
+
+    unit_collection = lines.split('\n\n')
+    unit_collection = [i.split('\n') for i in unit_collection]
+    unit_collection = convert_data_to_int(unit_collection)
+    return unit_collection
+
+def calculate_calories(data_file: Path)-> List[int]:
+
+    calories_sum: list = []
+    calories_collection = loade_input_file(data_file=data_file)
+    for number, calories in enumerate(calories_collection):
+        calories_sum.append(sum(calories))
+    calories_sum.sort()
+    return calories_sum
+
+def get_most(units_to_get: int, data_file: Path) -> List[int]:
+    calories_list = calculate_calories(data_file=data_file)
+    calories_list.reverse()
+    return calories_list[0:units_to_get]
--- Day_1/test_most_calories.py
+++ Day_1/test_most_calories.py
@@ -1,6 +1,13 @@
 from get_most_calories import get_most
 from pathlib import Path
+import os
 
 def test_get_most_calories():
-    assert get_most(Path('Day_1/test_data.txt')) == 4
-    print(get_most(Path('Day_1/input.txt')))
+    assert get_most(units_to_get=1, data_file=Path(os.path.join('advent_of_code_2022\Day_1', 'test_data.txt')))[0] == 24000
+
+def test_get_calories_top_tre():
+    assert get_most(units_to_get=3, data_file=Path(os.path.join('advent_of_code_2022\Day_1', 'test_data.txt'))) == [24000, 11000, 6000]
+
+def test_sum_tre_most():
+    tre_most = get_most(units_to_get=3, data_file=Path(os.path.join('advent_of_code_2022\Day_1', 'input2.txt')))
+    assert sum(tre_most) == 208437
